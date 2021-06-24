04136c9e470b339aecd4b3035ae6ee37e1aefa55
Alejahndro <alejahndro02@gmail.com>
2021-06-24 13:26:09 -0500

    Se declaro la variable firebase co los datos correspondientes a uesto proyecto genrado e firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,  
+  firebase: {
+    apiKey: "AIzaSyCAG8A70h7vFnstVJNneFQKz-_-qd24YC8",
+    authDomain: "tarjetacredito-f6b91.firebaseapp.com",
+    projectId: "tarjetacredito-f6b91",
+    storageBucket: "tarjetacredito-f6b91.appspot.com",
+    messagingSenderId: "44204289827",
+    appId: "1:44204289827:web:0e8447bf46b0da72cd6696"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCAG8A70h7vFnstVJNneFQKz-_-qd24YC8",
+    authDomain: "tarjetacredito-f6b91.firebaseapp.com",
+    projectId: "tarjetacredito-f6b91",
+    storageBucket: "tarjetacredito-f6b91.appspot.com",
+    messagingSenderId: "44204289827",
+    appId: "1:44204289827:web:0e8447bf46b0da72cd6696"
+  }
 };
 
 /*
