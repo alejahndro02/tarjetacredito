f1633428f960e331caee071e880ec889ba2bcd92
Alejahndro <alejahndro02@gmail.com>
2021-06-24 14:38:53 -0500

    Se importo el modelo de TarjetaCredito para obtener los datos provenientes del html y guardar la informacion con el metodo guardar datos rn la data de firebase atravez del modulo ANgularFirestore

--- src/app/services/tarjeta.service.ts
+++ src/app/services/tarjeta.service.ts
@@ -1,9 +1,15 @@
+import { TarjetaCredito } from './../models/TarjetaCredito';
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarjetaService {
 
-  constructor() { }
+  constructor(private firebase:AngularFirestore) { }
+  
+  guardarDatos=(tarjeta:TarjetaCredito):Promise<any>=>{
+    return this.firebase.collection('tarjetas').add(tarjeta)
+  }
 }
