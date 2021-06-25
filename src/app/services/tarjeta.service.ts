d9be80541f5b0db898f00580d781b2060cc8b658
Alejahndro <alejahndro02@gmail.com>
2021-06-24 19:58:39 -0500

    Se crea un nuevo metodo com observable y esta apuntando a la coleccion de la data en firebase

--- src/app/services/tarjeta.service.ts
+++ src/app/services/tarjeta.service.ts
@@ -1,6 +1,7 @@
 import { TarjetaCredito } from './../models/TarjetaCredito';
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,8 @@ export class TarjetaService {
   
   guardarDatos=(tarjeta:TarjetaCredito):Promise<any>=>{
     return this.firebase.collection('tarjetas').add(tarjeta)
+  }
+  obtenerDatos=():Observable<any>=>{
+    return this.firebase.collection('tarjetas').snapshotChanges();
   }
 }
