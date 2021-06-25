4c8ee60604a82316a203af22288d3da0f19ec4bd
Alejahndro <alejahndro02@gmail.com>
2021-06-25 11:26:49 -0500

    Se declararon los metodos addTarjetaEdit y getTarjetaEdit que es un observabkle y estara apuntando a la base de firebase

--- src/app/services/tarjeta.service.ts
+++ src/app/services/tarjeta.service.ts
@@ -1,22 +1,28 @@
 import { TarjetaCredito } from './../models/TarjetaCredito';
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarjetaService {
-
+  private tarjeta$= new Subject<any>()
   constructor(private firestore:AngularFirestore) { }
   
   guardarDatos(tarjeta:TarjetaCredito):Promise<any>{
-    return this.firestore.collection('tarjetas').add(tarjeta)
+    return this.firestore.collection('tarjetas').add(tarjeta);
   }
   obtenerDatos():Observable<any>{
     return this.firestore .collection('tarjetas', ref => ref.orderBy('fechaCreacion', 'asc')).snapshotChanges();
   }
   eliminarDatos(id:string):Promise<any>{
-    return this.firestore.collection('tarjetas').doc(id).delete().then()
+    return this.firestore.collection('tarjetas').doc(id).delete().then();
+  }
+  addTajetaEdit(tarjeta:TarjetaCredito){
+    this.tarjeta$.next(tarjeta);
+  }
+  getTarjetaEdit():Observable<TarjetaCredito>{
+    return this.tarjeta$.asObservable();
   }
 }
